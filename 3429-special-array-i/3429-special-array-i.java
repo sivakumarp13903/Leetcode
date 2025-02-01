c2531ccf21b498d4d14666e190f6f9e848560dca
Sivakumar <125781617+sivakumarp13903@users.noreply.github.com>
2025-02-01 05:42:44 +0530

    Time: 1 ms (37.58%), Space: 43 MB (93.15%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        if(nums.length<2){
+            return true;
+        }
+        for(int i=1;i<nums.length;i++){
+            if((nums[i-1]%2==0 && nums[i]%2!=0) || (nums[i-1]%2!=0 && nums[i]%2==0) ){
+                continue;
+            }
+            else{
+                return false;
+            }
+        }return true;
+    }
+}
